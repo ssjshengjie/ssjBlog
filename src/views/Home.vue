2f938c05c15780852ddbf309ec752e5aebb954cf
ssjshengjie <13127202636@163.com>
2020-08-15 16:18:03 +0800

    1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,78 +1,111 @@
 <template>
-  <div class="blog">
+  <div class="blog" @click="handleScroll">
     <!-- 头部----------------------------------------->
-    <div class="blog_header" id="searchBar">
-        
-      <nav class="blog_header_nav" :class="NavBlogBarFixed == true ? 'isFixed' : ''">
-        <!-- 博客LOGO -->
-        <div class="blog_header_nav_left">
-          <div class="blog_header_nav_left_img">ssjのblog</div>
-        </div>
-        <div class="blog_header_nav_right">
-          <div class="blog_nav_index">
-            <div class="v_blog_nav_index">
-              <span class="el-icon-s-home"></span>
-              <span class="item-text">
-                <router-link to="index">首页</router-link>
-              </span>
-            </div>
-          </div>
-          <div class="blog_nav_file">
-            <div class="v_blog_nav_file">
-              <span class="el-icon-s-order"></span>
-              <span class="item-text">
-                <router-link to="file">归档</router-link>
-              </span>
-            </div>
-          </div>
-          <div class="blog_nav_link">
-            <div class="v_blog_nav_link">
-              <span class="el-icon-s-promotion"></span>
-              <span class="item-text">
-                <router-link to="link">链接</router-link>
-              </span>
-            </div>
-          </div>
-          <div class="blog_nav_about">
-            <div class="v_blog_nav_about">
-              <span class="el-icon-collection"></span>
-              <span class="item-text">
-                <router-link to="about">关于</router-link>
-              </span>
-            </div>
+    <div class="blog-header" v-show="NavBlogBarFixed=true">
+      <!-- 博客LOGO -->
+      <div class="blog-header-nav">
+        <div class="blog-nav-index">
+          <div class="v-blog-nav-index">
+            <span class="el-icon-house"></span>
+            <span class="item-text">
+              <router-link to="index">首页</router-link>
+            </span>
           </div>
         </div>
-      </nav>
+        <div class="blog-nav-file">
+          <div class="v-blog-nav-file">
+            <span class="el-icon-collection"></span>
+            <span class="item-text">
+              <router-link to="file">归档</router-link>
+            </span>
+          </div>
+        </div>
+        <div class="blog-nav-link">
+          <div class="v-blog-nav-link">
+            <span class="el-icon-link"></span>
+            <span class="item-text">
+              <router-link to="link">链接</router-link>
+            </span>
+          </div>
+        </div>
+        <div class="blog-nav-about">
+          <div class="v-blog-nav-about">
+            <span class="el-icon-copy-document"></span>
+            <span class="item-text">
+              <router-link to="about">关于</router-link>
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
-
+    <nav class="blog-header-nav-hiddn" v-show="NavBlogBarFixed=false">
+      <!-- 博客LOGO -->
+      <div class="bblog-header-nav-left">
+        <div class="bblog-header-nav-left-img">ssjのblog</div>
+      </div>
+      <div class="bblog-header-nav-right">
+        <div class="blog-nav-index">
+          <div class="v-blog-nav-index">
+            <span class="el-icon-s-home"></span>
+            <span class="item-text">
+              <router-link to="index">首页</router-link>
+            </span>
+          </div>
+        </div>
+        <div class="blog-nav-file">
+          <div class="v-blog-nav-file">
+            <span class="el-icon-s-order"></span>
+            <span class="item-text">
+              <router-link to="file">归档</router-link>
+            </span>
+          </div>
+        </div>
+        <div class="blog-nav-link">
+          <div class="v-blog-nav-link">
+            <span class="el-icon-s-promotion"></span>
+            <span class="item-text">
+              <router-link to="link">链接</router-link>
+            </span>
+          </div>
+        </div>
+        <div class="blog-nav-about">
+          <div class="v-blog-nav-about">
+            <span class="el-icon-collection"></span>
+            <span class="item-text">
+              <router-link to="about">关于</router-link>
+            </span>
+          </div>
+        </div>
+      </div>
+    </nav>
     <!-- 中间部分---------------------------------- -->
-    <div class="blog_content">
-      <div class="blog_content_left"></div>
-      <div class="blog_content_middle">
+    <div class="blog-content">
+      <div class="blog-content-left"></div>
+      <div class="blog-content-middle">
         <router-view></router-view>
       </div>
-      <div class="blog_content_right">
+      <div class="blog-content-right">
         <content-right></content-right>
       </div>
     </div>
 
     <!-- 尾部-------------------------------------- -->
-      <Footer/>
-<!--     <div class="returnTop"> 
-    </div> -->
+    <Footer />
+    <!--     <div class="returnTop"> 
+    </div>-->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import ContentRight from "../components/ContentRight";
-import Footer from "../components/Fotter"
+import Footer from "../components/Fotter";
 export default {
   name: "Home",
-  components: { ContentRight,Footer},
+  components: { ContentRight, Footer },
   data() {
     return {
-      NavBlogBarFixed: false,
+      NavBlogBarFixed: true,
     };
   },
 
@@ -87,10 +120,7 @@ export default {
         window.pageYOffset ||
         document.documentElement.scrollTop ||
         document.body.scrollTop;
-      let offsetTop = document.querySelector("#searchBar").offsetTop;
-      scrollTop > offsetTop
-        ? (this.NavBlogBarFixed = true)
-        : (this.NavBlogBarFixed = false);
+      scrollTop > 500 ? (this.NavBlogBarFixed = true):(this.NavBlogBarFixed = false);
     },
   },
 };
@@ -111,105 +141,98 @@ a {
   height: 100%;
   overflow-x: hidden;
   /* 头部 */
-  .blog_header {
+  .blog-header {
     flex: 1 1 auto;
-   /*  background: #006699; */
-    height: 750px;
     display: flex;
-    flex-flow: column;
-    background: url("../assets/image/background.jpg");
-    background-repeat: no-repeat;
-    background-size:100% 100%;
-    /* 固定头部 */
-    .isFixed {
-      position: fixed;
-      top: 0;
-      z-index: 999;
-    }
-    /* 头部导航 */
-    .blog_header_nav {
+    height: 750px;
+    width: 100%;
+    border-bottom: 1px solid #cccccc;
+    .blog-header-nav {
       display: flex;
-      width: 100%;
-      height: 60px;
-      line-height: 60px;
-      border-bottom: 1px solid #dcdfe6;
-      background:rgba(255, 254, 153, 0.1);
-      cursor: pointer;
-      /* 导航左边 */
-      .blog_header_nav_left {
-        flex: 2;
-        .blog_header_nav_left_img {
-          padding-left: 240px;
-        }
+      flex: 1;
+      position: relative;
+      top: 75%;
+      left: 40%;
+      .blog-nav-index,
+      .blog-nav-file,
+      .blog-nav-link,
+      .blog-nav-about {
+        width: 80px;
+        height: 36px;
+        border: 1px solid #cccc;
+        border-radius: 10em;
+        background: #cccc;
+        margin: 10px;
+        text-align: center;
+        line-height: 36px;
       }
-      /* 导航右边 */
-      .blog_header_nav_right {
-        flex: 4;
-        display: flex;
-        align-items: center;
-        /*   justify-content: center; */
-        .blog_nav_index,
-        .blog_nav_file,
-        .blog_nav_link,
-        .blog_nav_about {
-          color: #828282;
-          font-size: 18px;
-          padding-left: 80px;
-          /* 鼠标移动伪元素 */
-          .v_blog_nav_index:hover,
-          .v_blog_nav_file:hover,
-          .v_blog_nav_link:hover,
-          .v_blog_nav_about:hover {
-            height: 30px;
-            line-height: 40px;
-            padding-bottom: 10px;
-            border-bottom: 1px solid rgba(64, 158, 255, 0.89);
-            .item-text:hover {
-              color: rgba(64, 158, 255, 0.89);
-            }
+    }
+  }
+  .blog-header-nav-hiddn{
+    display: none;
+    display: flex;
+    width: 100%;
+    height: 60px;
+    line-height: 60px;
+    border-bottom: 1px solid #dcdfe6;
+    background: rgba(255, 254, 153, 0.1);
+    cursor: pointer;
+    /* 导航左边 */
+    .bblog-header-nav-left {
+      flex: 2;
+      .bblog-header-nav-left-img {
+        padding-left: 240px;
+      }
+    }
+    /* 导航右边 */
+    .bblog-header-nav-right {
+      flex: 4;
+      display: flex;
+      align-items: center;
+      /*   justify-content: center; */
+      .blog-nav-index,
+      .blog-nav-file,
+      .blog-nav-link,
+      .blog-nav-about {
+        color: #828282;
+        font-size: 18px;
+        padding-left: 80px;
+        /* 鼠标移动伪元素 */
+        .v-blog-nav-index:hover,
+        .v-blog-nav-file:hover,
+        .v-blog-nav-link:hover,
+        .v-blog-nav-about:hover {
+          height: 30px;
+          line-height: 40px;
+          padding-bottom: 10px;
+          border-bottom: 1px solid rgba(64, 158, 255, 0.89);
+          .item-text:hover {
+            color: rgba(64, 158, 255, 0.89);
           }
         }
       }
     }
   }
+
   /* 中间部分 */
-  .blog_content {
+  .blog-content {
     width: 100%;
     height: auto;
     min-height: 75vh;
-    /*  min-height: 80vh; */
     display: flex;
     /* 中间左侧 */
-    .blog_content_left {
+    .blog-content-left {
       flex: 0 0 15%;
-      /*  background: #ff0; */
     }
     /* 中间右侧 */
-    .blog_content_right {
+    .blog-content-right {
       flex: 0 0 20%;
-      /* background: #cccccc; */
     }
     /* 中间主体 */
-    .blog_content_middle {
+    .blog-content-middle {
       flex: 1 1 auto;
       display: flex;
-      /*height: 100%;*/
-      /*  background: #00f; */
     }
-  }
-  /* 尾部 */
- /*  .blog_footer {
-    flex: 1 1 auto;
-    background: black;
-    height: 300px;
-  } */
-  .returnTop {
-    right: 38px;
-    bottom: 20px;
-    background: url('../assets/image/taibai.jpg');
-     width: 300px;
-    height: 200px;
-    position: fixed;
   }
 }
 </style>
